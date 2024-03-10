bfdf9a210c3f7adb0d12e78b60a3887f55b7c1ab
Ivan Borisov <BaHoRU2015@ya.ru>
2024-03-10 14:18:30 +0300

    updated start.sh and start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,2 @@
-python hw_boolean_search.py ^
-    --queries_file /work_dir/data/queries.numerate.txt ^
-    --objects_file  /work_dir/data/objects.numerate.txt ^
-    --docs_file /work_dir/data/docs.txt ^
-    --submission_file /work_dir/output.csv
+docker build -t boolean-search .
+docker run -it boolean-search
