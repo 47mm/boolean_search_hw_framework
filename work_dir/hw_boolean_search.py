ef627eb729851665244c15c0f569f66b8bbe6926
Ivan Borisov <BaHoRU2015@ya.ru>
2024-03-12 12:46:31 +0300

    new parser with score 0.45

--- work_dir/hw_boolean_search.py
+++ work_dir/hw_boolean_search.py
@@ -7,11 +7,9 @@ import re
 
 import numpy as np
 from natasha import (Segmenter, MorphVocab, NewsEmbedding, NewsMorphTagger, Doc)
-import nltk
-nltk.download('stopwords')
+
 import pandas as pd
 from tqdm import tqdm
-import pickle
 
 
 class Index:
@@ -20,11 +18,9 @@ class Index:
         self.morph_vocab = MorphVocab()
         emb = NewsEmbedding()
         self.morph_tagger = NewsMorphTagger(emb)
-        self.parts_of_speech = ['PROPN', 'ADJ', 'NOUN', 'X', 'VERB', 'NUM']
-        self.stop_words = nltk.corpus.stopwords.words('russian') + nltk.corpus.stopwords.words('english')
         self.inverted_index = {}
         with open(index_file) as file:
-            for line in tqdm(file, desc='Creating Index'):
+            for line in tqdm(file, desc='Creating Index...'):
                 first_tab = line.find('\t')
                 doc_id = int(line[1:first_tab])
                 text = self._preproccess_line(line[first_tab + 2:])
@@ -33,10 +29,11 @@ class Index:
                         self.inverted_index[word].add(doc_id)
                     else:
                         self.inverted_index[word] = {doc_id}
-        with open('saved_dictionary.pkl', 'wb') as f:
-            pickle.dump(self.inverted_index, f)
+        # with open('saved_dictionary.pkl', 'wb') as f:
+        #    pickle.dump(self.inverted_index, f)
+
         # with open('saved_dictionary.pkl', 'rb') as f:
-        #   self.inverted_index = pickle.load(f)
+        #    self.inverted_index = pickle.load(f)
 
     def _preproccess_line(self, line):
         s = re.sub(r'[^A-ZА-Я0-9 ]', '', line)
@@ -47,8 +44,7 @@ class Index:
             token.lemmatize(self.morph_vocab)
         tokens = []
         for token in doc.tokens:
-            if token.pos in self.parts_of_speech and token.lemma not in self.stop_words:
-                tokens.append(token.text)
+            tokens.append(token.lemma.upper())
         return tokens
 
     def get_index(self):
@@ -59,19 +55,61 @@ class QueryTree:
     def __init__(self, qid, query):
         self.qid = qid
         self.query = query
+        self.result = None
+        self.reversed_index = None
+        self.segmenter = Segmenter()
+        self.morph_vocab = MorphVocab()
+        emb = NewsEmbedding()
+        self.morph_tagger = NewsMorphTagger(emb)
+
+    def _preproccess_line(self, line):
+        s = re.sub(r'[^A-ZА-Я0-9 ]', '', line)
+        doc = Doc(s)
+        doc.segment(self.segmenter)
+        doc.tag_morph(self.morph_tagger)
+        for token in doc.tokens:
+            token.lemmatize(self.morph_vocab)
+        tokens = []
+        for token in doc.tokens:
+            tokens.append(token.lemma.upper())
+        return ''.join(tokens)
 
     def search(self, index):
-        reversed_index = index.get_index()
-        print( self.qid, self._split(self.query, reversed_index))
-        return self.qid, self._split(self.query, reversed_index)
+        self.reversed_index = index.get_index()
+        return self.qid, self._S()
 
-    def _split(self, query, reversed_index):
-        results = None
-        bracket_block = 0
-        or_block = False
+    def _is_brackets(self, q):
+        if q[0] != '(':
+            return False
+        brackets = 0
+        for c in q[1:-1]:
+            if c == '(':
+                brackets += 1
+            if c == ')':
+                brackets -= 1
+            if brackets < 0:
+                return False
+        return brackets == 0
+
+    def _remove_brackets(self, q):
+        return q[1:-1]
+
+    def _is_primitive(self, q):
+        return not('|' in q or ' ' in q or '(' in q or ')' in q)
+
+    def _get_next_lexem(self, query, is_and_block):
         q = ''
+        bracket_block = 0
         for c in query:
             q += c
+            if c == ' ' and bracket_block == 0 and is_and_block:
+                q1 = q[:-1]
+                q = ''
+                yield q1
+            if c == '|' and bracket_block == 0 and not is_and_block:
+                q1 = q[:-1]
+                q = ''
+                yield q1
             if c == '(':
                 bracket_block += 1
                 continue
@@ -79,44 +117,48 @@ class QueryTree:
                 continue
             if c == ')':
                 bracket_block -= 1
-                if bracket_block == 0:
-                    postings = self._split(q[1:-1], reversed_index)
-                    results = results & postings if results is not None else postings
-                    q = ''
                 continue
-            if c == '|' and not bracket_block:
-                if q[:-1]:
-                    postings = self._split(q[:-1], reversed_index)
-                    results = results | postings if results is not None else postings
-                    or_block = True
-                q = ''
-            if c == ' ' and not bracket_block and not or_block:
-                if q.strip():
-                    postings = self._split(q[:-1], reversed_index)
-                    results = results & postings if results is not None else postings
-                q = ''
+        yield q
+
+    def _is_and_block(self, query):
+        bracket_block = 0
+        for c in query:
+            if c == '|' and bracket_block == 0:
+                return False
+            if c == '(':
+                bracket_block += 1
                 continue
-            if c == ' ':
-                if not q.strip():
-                    or_block = False
-                    q = ''
-                continue
-        q = q.strip()
-        if ' ' in q:
-            for q_and in q.split(' '):
-                postings = reversed_index.get(q_and, set())
-                if or_block:
-                    results = results | postings if results is not None else postings
-                else:
-                    results = results & postings if results is not None else postings
+            if c == ')':
+                bracket_block -= 1
+        return True
+
+    def _add_results(self, result, postings, is_and_block):
+        if is_and_block:
+            result = result & postings if result is not None else postings
         else:
-            if q:
-                postings = reversed_index.get(q, set())
-                if or_block:
-                    results = results | postings if results is not None else postings
-                else:
-                    results = results & postings if results is not None else postings
-        return results
+            result = result | postings if result is not None else postings
+        return result
+
+    def _or_and_block(self, sequence, is_and_block):
+        result = None
+
+        for q in self._get_next_lexem(sequence, is_and_block):
+            if self._is_brackets(q):
+                q = self._remove_brackets(q)
+            if self._is_primitive(q):
+                q1 = self._preproccess_line(q)
+                postings = self.reversed_index.get(q1, set())
+                result = self._add_results(result, postings, is_and_block)
+                continue
+
+            postings = self._or_and_block(q, self._is_and_block(q))
+            result = self._add_results(result, postings, is_and_block)
+        return result
+
+    def _S(self):
+        result = self._or_and_block(self.query, True)
+        return result
+
 
 
 class SearchResults:
@@ -136,8 +178,9 @@ class SearchResults:
         ans = pd.DataFrame({
             'ObjectId': 1 + np.arange(len(ans)),
             'Relevance': ans,
-            })
+        })
         ans.to_csv(submission_file, index=False)
+
 
 def main():
     # Command line arguments.
@@ -154,7 +197,7 @@ def main():
     # Process queries.
     search_results = SearchResults()
     with codecs.open(args.queries_file, mode='r', encoding='utf-8') as queries_fh:
-        for line in queries_fh:
+        for line in tqdm(queries_fh, desc='Processing queries...'):
             fields = line.rstrip('\n').split('\t')
             qid = int(fields[0])
             query = fields[1]
